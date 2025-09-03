d49c7c92ef85c0b85b4a70963e42c0901e0bda6d
yiyinqi <yiyinqi@berkeley.edu>
2025-09-02 17:50:09 -0700

    fixed

--- src/pages/cs180project0.jsx
+++ src/pages/cs180project0.jsx
@@ -19,22 +19,18 @@ const CS180Proj0 = () => {
             <h2>Part 2: Architectural Perspective Compression</h2>
             <div className="photo-row">
                 <div className="photo">
-                    <img src="/streetnozoom.png" alt="No zoom, close up" />
+                    <img src="/buildingnozoom.png" alt="No zoom, close up" />
                     <p>No zoom, close up</p>
                 </div>
                 <div className="photo">
-                    <img src="/streetzoomedin.png" alt="Zoomed in, far away" />
+                    <img src="/buildingzoomedin.png" alt="Zoomed in, far away" />
                     <p>Zoomed in, far away</p>
                 </div>
             </div>
             <h2>Part 3: The Dolly Zoom</h2>
             <div className="photo">
-                <img src="/oncampus.gif" alt="the dolly zoom on campus" />
-                <p>Enjoying the view on campus</p>
-            </div>
-            <div className="photo">
-                <img src="/inapt.gif" alt="the dolly zoom in my apartment" />
-                <p>Enjoying the view in my apartment</p>
+                <img src="/lamppost.gif" alt="dolly zoom lamppost" />
+                <p>lamppost</p>
             </div>
         </div>
     )
